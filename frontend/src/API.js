7054d8e55b4f66f3cb8a5ebf48c2ad0f71003ad9
Vipul-2710 <vipulkayate@gmail.com>
2022-09-05 22:45:17 +0530

    updated files

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -7,7 +7,7 @@ if (
   process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
 ) {
   baseURL = process.env.REACT_APP_API_BASE_URL;
-} else baseURL = "";
+} else baseURL = "https://tr-tea-shop-backend.herokuapp.com/";
 
 const api = axios.create({
   baseURL: baseURL,
@@ -173,43 +173,43 @@ export default class API {
   // Reference Post
   //////////////////////////////////////////
 
-  getPosts = async () => {
-    const posts = await api
-      .get("/posts/")
-      .then((response) => {
-        return response.data;
-      })
-      .catch((error) => {
-        throw new Error(error);
-      });
-    return posts;
-  };
+  // getPosts = async () => {
+  //   const posts = await api
+  //     .get("/posts/")
+  //     .then((response) => {
+  //       return response.data;
+  //     })
+  //     .catch((error) => {
+  //       throw new Error(error);
+  //     });
+  //   return posts;
+  // };
 
-  addPost = async (name, body, image) => {
-    const formData = new FormData();
-    formData.append("name", name);
-    formData.append("body", body);
-    formData.append("image", image);
-    const savedPost = await api
-      .post("/posts/add/", formData)
-      .then((response) => {
-        return response.data;
-      })
-      .catch((error) => {
-        throw new Error(error);
-      });
-    return savedPost;
-  };
+  // addPost = async (name, body, image) => {
+  //   const formData = new FormData();
+  //   formData.append("name", name);
+  //   formData.append("body", body);
+  //   formData.append("image", image);
+  //   const savedPost = await api
+  //     .post("/posts/add/", formData)
+  //     .then((response) => {
+  //       return response.data;
+  //     })
+  //     .catch((error) => {
+  //       throw new Error(error);
+  //     });
+  //   return savedPost;
+  // };
 
-  deletePost = async (id) => {
-    const response = await api
-      .delete("/posts/delete/" + id + "/")
-      .then((response) => {
-        return response.data;
-      })
-      .catch((error) => {
-        throw new Error(error);
-      });
-    return response;
-  };
+  // deletePost = async (id) => {
+  //   const response = await api
+  //     .delete("/posts/delete/" + id + "/")
+  //     .then((response) => {
+  //       return response.data;
+  //     })
+  //     .catch((error) => {
+  //       throw new Error(error);
+  //     });
+  //   return response;
+  // };
 }
